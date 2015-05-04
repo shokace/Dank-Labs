55285050518a69290f3acf179254aab1eddcf33b
crabbekid <kevincrabbe@me.com>
2015-05-03 21:58:15 -0700

    Update 6c(prof. must look)

--- Lab 6/DynamicArray.cpp
+++ Lab 6/DynamicArray.cpp
@@ -1,81 +1,32 @@
-template <class DataType>
-Array<DataType>::DynamicArray(int size)
+#include <iostream>
+#include "DynamicArray.h"
+using namespace std;
+
+int main()
 {
-	if (size < 1)
+	//prompt the user to enter the values and create the dynamic arr
+	DynamicArray<int> arr;
+	int numVal = 0;
+	while (true)
 	{
-		capacity = 1;
-		errorCode = 1;
+		int input;
+		cout << "Please enter up to one hundred integers." << endl;
+		cout << "Enter an integer or 'Q' to finish: ";
+		cin >> input;
+		if (input == 'q' || input == 'Q')//this happens after data entry is complete
+		{
+			cout << numVal << " values were entered." << endl;
+			for (int i = 0; i < numVal; i++)
+			{
+				cout << arr[i] << ", ";
+			}
+			cout << endl;
+		}
+		//push value onto dynamic array
+		else
+		{
+			arr[numVal] = input;
+			numVal++;
+		}
 	}
-
-	else
-	{
-		capcity = size;
-		errorCode = 0;
-	}
-
-	elements = new DataType[capacity];
-}
-
-template <class DataType>
-inline DataType & Array<DataType>::operator[](int index)
-{
-#ifndef DEBUG_ARRAY
-	if (index < 0 || index >= capacity)//??
-	{
-		errorCode |= 2;
-		return dud;
-	}
-#endif
-	return elements[index];
-}
-
-template <class DataType>
-void Array<DataType>::changeSize(int newSize)
-{
-	if (newSize < 1)
-	{
-		errorCode |= 4;
-		return;
-	}
-
-	DataType *newArray = new DataType[newSize];
-
-	int limit = (newSize > capacity) ? capacity : newSize;
-
-	for (int i = 0; i < limit; i++)
-		newArray[i] = elements[i];
-	
-	delete[] elements;
-
-	elements = newArray;
-
-	capacity = newSize;
-}
-
-template <class DataType>
-inline int Array<DataType>::length() const
-{
-	return capacity;
-}
-
-template <class DataType>
-string Array<DataType>::err()const
-{
-	if (errorCode == 0)
-		return "No error.\n";
-	
-	string errorMessage = "";
-	if (errorCode & 1)
-	{
-		errorMessage += "Nonpositive size passed into constructor, so\n";
-		errorMessage += "the capacity was set to 1 by default.\n";
-	}
-	if (errorCode & 2)
-		errorMessage += "Index out of range.\n";
-	if (errorCode & 4)
-	{
-		errorMessage += "Nonpositive size passed into the changeSize, so\n";
-		errorMessage += "the size of the array was not changed.\n";
-	}
-	return errorMessage;
 }
