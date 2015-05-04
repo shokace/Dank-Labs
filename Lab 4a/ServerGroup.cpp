dbbc5385de96956c3bf017e9cdac0edd6d46c06e
crabbekid <kevincrabbe@me.com>
2015-05-03 20:42:20 -0700

    Update ServerGroup.cpp

--- Lab 4a/ServerGroup.cpp
+++ Lab 4a/ServerGroup.cpp
@@ -1,53 +1,76 @@
-// ServerGroup.cpp Class Implementation File
-
 #include "ServerGroup.h"
+#include <iostream>
+using namespace std;
 
-ServerGroup::ServerGroup( const int & num )
+ServerGroup::ServerGroup(int num) //starts by creating dynamic array with size based on int val
 {
-  size = num;
-  int *servers = new int [ size ];
-  for(int i=0; i<size; i++)
-  {
-    servers[ i ] = 0;
-  }
-  freeServer = 0;
-  spServer = 0;
+	//point to new array in heap
+	ptr = new int[num];
+	size = num;
+
+	//set all elements of array to 0
+	for (int i = 0; i < size; i++)
+	{
+		ptr[i] = 0;
+	}
+
+	//set spServer to 0(there is only one sp server)
+	spServer = 0;
 }
 
-bool ServerGroup::spServerFree()
+bool ServerGroup::spServerFree()//true if spServer = 0; else false;called by usespServer
 {
-  if (spServer = 0) return true;
-  else return false;
+	if (spServer == 0)
+		return true;
+	else
+		return false;
 }
 
-bool ServerGroup::serverFree()  {
-  for (int i=0; i<size; i++)
-  {
-    if ( servers[ i ] = 0)
-    {
-      freeServer = i;
-      return true;
-    }
-  }
-  return false;
+bool ServerGroup::serverFree()
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		if (ptr[i] == 0)
+		{
+			freeServer = i;
+			return true;
+		}
+	}
+	return false;
 }
 
-void ServerGroup::useServer( const int & avTransTime )
+void ServerGroup::useServer(int avTransTime)
 {
-  servers[ freeServer ] = avTransTime;
+	ptr[freeServer] = avTransTime;
 }
 
-void ServerGroup::usespServer( const int & avTransTime )
+void ServerGroup::usespServer(int avTransTime)
 {
-  spServer = avTransTime;
+	spServer = avTransTime;
 }
 
-void ServerGroup::decServers()
+void ServerGroup::decServer()
 {
-  if ( spServer > 0) spServer--;
+	//decrement spServer by 1 unless already 0
+	if (spServer > 0)
+		spServer--;
 
-  for (int i=0; i<size; i++)
-  {
-    if( servers[ i ] > 0) servers[ i ]--;
-  }
+	for (int i = 0; i < size; i++)
+	{
+		if (ptr[i] > 0)
+			ptr[i]--;
+	}
+
+}
+
+void ServerGroup::displayServers()
+{
+	cout << "Servers: \n";
+	cout << "[ ";
+	for (int i = 0; i < size; i++)
+		cout << " " << ptr[i];
+	cout << " ]" << endl;
+
+	cout << "Special Server:";
+	cout << spServer <<endl;
 }
