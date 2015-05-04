6e28cf462f4f31c23bf2b5f919a80bbcc1c992ed
crabbekid <kevincrabbe@me.com>
2015-05-03 20:41:31 -0700

    Update ServerGroup.h
    Final

--- Lab 4a/ServerGroup.h
+++ Lab 4a/ServerGroup.h
@@ -1,27 +1,24 @@
-// ServerGroup header file
-
 #ifndef SERVERGROUP_H
 #define SERVERGROUP_H
+
 class ServerGroup
 {
-public:
-
-  ServerGroup(const int & num );
-
-  bool spServerFree();
-  bool serverFree();
-  void useServer( const int & avTransTime );
-  void usespServer( const int & avTransTime );
-  void decServers();
-
-  int printServer( int num )
-  {
-    return servers[ num ];
-  }
-
 private:
-  int *servers;
-  int spServer, freeServer, size;
+	
+	int *ptr;//pointer to dynamic integer array called servers(will have to be declared in main?
+	int spServer;
+	int freeServer;
+	int size;
 
-  };
+public:
+	ServerGroup();
+	ServerGroup(int);
+	bool spServerFree();//will return true if 0 is found in array or will return false otherwise(based on whether special purpose server is free)
+	bool serverFree();// will return true if 0 is found in array or will return false otherwise(based on whether server is free) will set freeServer to index of array
+	void useServer(int);//sets servers[freeServer] to avTransTime which is average transaction time to serve user in line
+	void usespServer(int);//sets spServer to avTransTime
+	void decServer();
+	void displayServers();
+
+};
 #endif
